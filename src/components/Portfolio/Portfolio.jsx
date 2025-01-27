ae61f8e2569b467efaa2de4d1944b00dcc4410af
Pprogram432 <pro.thanadech@gmail.com>
2025-01-27 13:47:42 +0700

    ย้ายไฟล์รูปภาพออกจาก src เปลี่ยน คำอธิบาย portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -9,39 +9,39 @@ function Portfolio() {
         <div className={styles.port_list}>
             <div className={styles.port_items}>
                 <Tilt scale={1.1} transitionSpeed={1500} tiltReverse={true}>
-                <img src="./src/img/portfolio6.png" alt="" />
+                <img src="/img/portfolio6.png" alt="" />
                 </Tilt>
-                <p>Project Tiltle</p>
+                <p>7-day activity schedule</p>
             </div>
             <div className={styles.port_items}>
                 <Tilt scale={1.1} transitionSpeed={2000} tiltReverse={true}>
-                <img src="./src/img/portfolio5.png" alt="" />
+                <img src="/img/portfolio5.png" alt="" />
                 </Tilt>
-                <p>Project Tiltle</p>
+                <p>Database</p>
             </div>
             <div className={styles.port_items}>
                 <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
-                <img src="./src/img/portfolio4.png" alt="" />
+                <img src="/img/portfolio4.png" alt="" />
                 </Tilt>
-                <p>Project Tiltle</p>
+                <p>Matchtee project</p>
             </div>
             <div className={styles.port_items}>
                 <Tilt scale={1.1} transitionSpeed={3000} tiltReverse={true}>
-                <img src="./src/img/portfolio3.png" alt="" />
+                <img src="/img/portfolio3.png" alt="" />
                 </Tilt>
-                <p>Project Tiltle</p>
+                <p>Information Form</p>
             </div>
             <div className={styles.port_items}>
                 <Tilt scale={1.1} transitionSpeed={3500} tiltReverse={true}>
-                <img src="./src/img/portfolio2.png" alt="" />
+                <img src="/img/portfolio2.png" alt="" />
                 </Tilt>
-                <p>Project Tiltle</p>
+                <p>Academic Grade</p>
             </div>
             <div className={styles.port_items}>
                 <Tilt scale={1.1} transitionSpeed={4000} tiltReverse={true}>
-                <img src="./src/img/portfolio1.png" alt="" />
+                <img src="/img/portfolio1.png" alt="" />
                 </Tilt>
-                <p>Project Tiltle</p>
+                <p>Exercise</p>
             </div>
         </div>      
     </div>
