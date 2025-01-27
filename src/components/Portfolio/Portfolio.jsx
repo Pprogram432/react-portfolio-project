3adc65b481456bda728e6329ea7ebe2179c9e6d5
Pprogram432 <pro.thanadech@gmail.com>
2025-01-27 13:18:56 +0700

    เพิ่มจุดใน path ไฟล์รูปภาพ

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -9,37 +9,37 @@ function Portfolio() {
         <div className={styles.port_list}>
             <div className={styles.port_items}>
                 <Tilt scale={1.1} transitionSpeed={1500} tiltReverse={true}>
-                <img src="/src/img/portfolio6.png" alt="" />
+                <img src="./src/img/portfolio6.png" alt="" />
                 </Tilt>
                 <p>Project Tiltle</p>
             </div>
             <div className={styles.port_items}>
                 <Tilt scale={1.1} transitionSpeed={2000} tiltReverse={true}>
-                <img src="/src/img/portfolio5.png" alt="" />
+                <img src="./src/img/portfolio5.png" alt="" />
                 </Tilt>
                 <p>Project Tiltle</p>
             </div>
             <div className={styles.port_items}>
                 <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
-                <img src="/src/img/portfolio4.png" alt="" />
+                <img src="./src/img/portfolio4.png" alt="" />
                 </Tilt>
                 <p>Project Tiltle</p>
             </div>
             <div className={styles.port_items}>
                 <Tilt scale={1.1} transitionSpeed={3000} tiltReverse={true}>
-                <img src="/src/img/portfolio3.png" alt="" />
+                <img src="./src/img/portfolio3.png" alt="" />
                 </Tilt>
                 <p>Project Tiltle</p>
             </div>
             <div className={styles.port_items}>
                 <Tilt scale={1.1} transitionSpeed={3500} tiltReverse={true}>
-                <img src="/src/img/portfolio2.png" alt="" />
+                <img src="./src/img/portfolio2.png" alt="" />
                 </Tilt>
                 <p>Project Tiltle</p>
             </div>
             <div className={styles.port_items}>
                 <Tilt scale={1.1} transitionSpeed={4000} tiltReverse={true}>
-                <img src="/src/img/portfolio1.png" alt="" />
+                <img src="./src/img/portfolio1.png" alt="" />
                 </Tilt>
                 <p>Project Tiltle</p>
             </div>
